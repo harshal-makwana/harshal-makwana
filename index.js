47fcce61d3f417149bf095f40e6c1f39b4123848
harshal <harshalmakwana84@gmail.com>
2023-03-28 19:25:45 +0530

    let's see what happens

--- index.js
+++ index.js
@@ -5,20 +5,21 @@ const app = express()
 
 const path = require('path')
 require('./routes/nodeMailer')
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 app.use(bodyParser.urlencoded({ extended : true}))
 // require('./routes/feedbackRoute')(app)
 
+
+app.set('view engine', 'ejs')
 app.use(express.static(__dirname + '/views/'))
 app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
 
 // app.use(express.urlencoded( {extended : true}))
 
 
 app.get('/', (req, res) => res.render('index.ejs') )
 app.get('/about', (req, res) => res.render('about.ejs') )
-app.get('/contact', (req, res) => res.render('contact.ejs' , {message : 'hello'}) )
+app.get('/contact', (req, res) => res.render('contact.ejs') )
 app.get('/works', (req, res) => res.render('works.ejs') )
 app.get('/work', (req, res) => res.render('work.ejs') )
 app.get('/sample', (req, res) => res.render('sample.ejs') )
--- routes/nodeMailer.js
+++ routes/nodeMailer.js
@@ -1,6 +1,6 @@
 
 const nodemailer = require('nodemailer')
-
+require('dotenv').config();
 
 module.exports = (app) => {
     
@@ -10,26 +10,20 @@ module.exports = (app) => {
           
             service: 'gmail',  // true for 465, false for other ports
             auth: {
-              user: 'harshalmakwana84@gmail.com', // generated ethereal user
-              pass: 'harshal333', // generated ethereal password
+              user: process.env.GMAIL_USER, // generated ethereal user
+              pass: process.env.GMAIL_PASS, // generated ethereal password
             },
           });
         
           // send mail with defined transport object
           let info = await transporter.sendMail({
             from: '"Fred Foo 👻" <harshalmakwana@protonmail.com>', // sender address
-            to: "bar@example.com, baz@example.com", // list of receivers
+            to: "harshalmakwana@protonmail.com", // list of receivers
             subject: "Hello ✔", // Subject line
             text: "Hello world?", // plain text body
             html: "<b>Hello world?</b>", // html body
           });
         
-          console.log("Message sent: %s", info.messageId);
-          // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-        
-          // Preview only available when sending through an Ethereal account
-          console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-          // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
         
         }
         
